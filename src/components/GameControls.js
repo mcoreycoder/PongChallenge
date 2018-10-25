2e8a76409ff09dc261c756f2bd8df20d0652e515
Michael Corey <fishincorey@gmail.com>
2018-10-25 13:39:06 -0600

    created dev branch and added form and inputs to GameControls

--- src/components/GameControls.js
+++ src/components/GameControls.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 const GameControls = props => {
 
+    //MC added contents in <article>
     return (
         <article>
-            
+            <form>
+                <label> Set the max score to win:</label>
+                <input type="number" />
+                <br />
+                <label> Set the initial velocity of the ball:</label>
+                <input type="number" />
+                <br />
+                <label> Paddle colors for players 1 and 2:</label>
+                <input type="color" />
+                <br />
+                <label> Ball color:</label>
+                <input type="color" />
+                <br />
+                <label> Ball shape:</label>
+                <input type="shape" />
+            </form>
+            <button type="button">Start Game</button>
         </article>
     )
 }
